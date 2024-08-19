f1bf501caab493a2b35b27b69e9d5b6991cee730
ItzOluf <156479216+ItzOluf@users.noreply.github.com>
2024-08-19 21:02:47 +0200

    added gravity function
    values not fully finsihed adjusting gravity is only working on y for now and can't be turned around

--- funny ballbouncepygame.py
+++ funny ballbouncepygame.py
@@ -4,21 +4,31 @@ import time
 import sys
 
 pygame.init()
-window_size = (450,1600/2)
-
+window_size = (450,800)
+rect_x =225
+rect_y = 400
 black = (0,0,0)
+var_gravity = 1
+
 screen = pygame.display.set_mode(window_size)
-pygame.display.set_caption('gravity')
-# Hauptprogrammschleife
+pygame.display.set_caption('ballls')
 running = True
+
+def gravity(x,y,mass):
+    y+= mass/5
+    print(y)
+    return x,y
+
 while running:
     screen.fill((15, 15, 15))
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
     screen.fill(black)
-    pygame.draw.circle(screen, (255,255,255), (300,200), 200, width=20)
-    pygame.draw.circle(screen, )
+    pygame.draw.circle(screen, (255,255,255), (225,400), 225, width=10)
+    pygame.draw.circle(screen, (255,0,0), (rect_x,rect_y),15)
+    rect_x, rect_y =gravity(rect_x,rect_y,1)
+
     pygame.display.flip()
     
 pygame.quit()
